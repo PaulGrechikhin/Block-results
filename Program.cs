b1c8b609e47fc12898ba375f08c9d08c4af51219
PaulGrechikhin <pgrechihin@mail.ru>
2023-04-23 18:32:21 +0300

    Fianal

--- Program.cs
+++ Program.cs
@@ -1,4 +1,43 @@
 ﻿Console.Clear();
 
 
-Console.WriteLine("Hello, World!");
+Console.Write("Задайте длину массива: ");
+bool flag = int.TryParse(Console.ReadLine()!, out int N);
+
+string[] Arr = new string[N];
+string[] ArrFinal = new string[Arr.Length];
+FillArray(Arr);
+PrintArray(Arr);
+Console.WriteLine();
+CreatArray(Arr, ArrFinal);
+
+
+
+
+string[] FillArray(string[] array)
+{
+    for (int i = 0; i < N; i++)
+    {
+       array[i] = Convert.ToString(Console.ReadLine()!);
+    }
+    return array;
+}
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        Console.WriteLine($"a[{i}] = {arr[i]}");
+}
+
+void CreatArray(string[] arr, string[] array)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i].Length <= 3)
+        {
+            array[i] = arr[i];
+            Console.WriteLine($"{array[i]}");
+        }
+    }
+    
+}
